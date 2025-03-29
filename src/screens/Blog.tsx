ef7c99d2cbe3860ebbe9ed731482bed6e32ecc54
Moshood Oluwatobi <oluwatobimoshood16@gmail.com>
2025-03-29 14:51:18 +0100

    included faqs

--- src/screens/Blog.tsx
+++ src/screens/Blog.tsx
@@ -5,8 +5,12 @@ import Container from "@/components/Container";
 import { useGetBlog } from "@/hooks/useGetBlog";
 import Image from "next/image";
 import { Blog } from "@/data";
+import TransitionLink from "@/components/TransitionLink";
+import { useGetFaq } from "@/hooks/useGetFaqs";
 
 const BlogsPage = () => {
+  const { data: faqs, isLoading } = useGetFaq();
+
   const { data: blogs, isLoading: isBlogLoading } = useGetBlog();
   const [blogList, setBlogList] = useState<Blog[]>([]);
 
@@ -58,6 +62,50 @@ const BlogsPage = () => {
                   title={post.title}
                 />
               ))}
+        </div>
+        <div className=" py-24">
+          <div className="mb-16">
+            <h2 className="mb-8 font-serif lg:text-4xl text-3xl font-light tracking-wide text-gray-900 md:text-4xl">
+              FAQ
+            </h2>
+            <div className="h-px w-full  bg-gray-200" />
+          </div>
+
+          <div className="grid gap-x-24 lg:px-10 gap-y-16 md:grid-cols-2">
+            {isLoading
+              ? Array.from({ length: 3 }).map((_, index) => (
+                  <div key={index} className="space-y-4 animate-pulse">
+                    <div className="h-6 bg-gray-300 rounded w-3/4" />
+                    <div className="h-4 bg-gray-300 rounded w-full" />
+                    <div className="h-4 bg-gray-300 rounded w-5/6" />
+                    <div className="h-4 bg-gray-300 rounded w-2/3" />
+                  </div>
+                ))
+              : faqs?.data.map((faq) => (
+                  <div key={faq.id} className="space-y-4">
+                    <h3 className="font-serif text-2xl font-light leading-snug text-gray-900">
+                      {faq.question}
+                    </h3>
+                    <p className="font-light leading-relaxed text-gray-600">
+                      {faq.answer}
+                    </p>
+                  </div>
+                ))}
+          </div>
+
+          <div className="my-24 h-px w-full max-w-[400px] mx-auto bg-gray-200" />
+
+          <div className="text-center">
+            <h3 className="mb-20 font-serif text-3xl font-light tracking-wide text-gray-900">
+              Ready to Begin?
+            </h3>
+            <TransitionLink
+              href="/contact"
+              className="inline-block font-serif bg-[#BEB3A7] px-8 py-3 text-sm font-light tracking-wide text-white transition-colors hover:bg-[#807c7c]"
+            >
+              Book a Session
+            </TransitionLink>
+          </div>
         </div>
       </Container>
     </div>
